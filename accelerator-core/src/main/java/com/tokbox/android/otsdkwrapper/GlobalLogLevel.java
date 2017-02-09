71b5c758e9f3d1b8522bffac8fa845f1f86136a8
Antonio M. Amaya Calvo <amac@tokbox.com>
2017-02-09 18:27:34 +0100

    [OTS-937] Change signal listening to a thread pool and make log customizable (#13)
    * [OTS-937] Process the signals on a Thread Pool
    
    * [OTS-937] Disable ThreadPool logs

---
+++ accelerator-core/src/main/java/com/tokbox/android/otsdkwrapper/GlobalLogLevel.java
@@ -0,0 +1,9 @@
+package com.tokbox.android.otsdkwrapper;
+
+/**
+ * Global log level for this module. Change it to the maximum log level you want to get for all
+ * modules.
+ */
+public class GlobalLogLevel {
+  public final static short MAX_LOG_LEVEL = 0xFF;
+}
